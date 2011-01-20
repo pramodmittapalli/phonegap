c023cdda4d0e21ae8012143a1119e16e2c342ee3
Geoff Kaile <geoff.kaile@gmail.com>
2011-01-20 18:04:07 +0000

    GPK - Cleaning up

--- www/static/js/utils.js
+++
@@ -1,8 +0,0 @@
-window.$ = {
-    loadJS : function(url,callback) {
-        var script = document.createElement('script');
-        script.src = url,
-        document.body.appendChild(script);
-        if (callback) script.onload = callback;
-    }
-};
